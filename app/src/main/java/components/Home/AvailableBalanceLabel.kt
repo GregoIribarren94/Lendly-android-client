8fd15e1520d40f784844ceb53ef75d2a21b43798
gregoiri <grego_922@hotmail.com>
2026-05-30 00:04:23 -0300

    componentes agregados

---
+++ app/src/main/java/components/Home/AvailableBalanceLabel.kt
@@ -0,0 +1,20 @@
+
+package com.lendly.fintech.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import com.lendly.fintech.ui.theme.CaptionMedium
+import com.lendly.fintech.ui.theme.ContentSecondary
+
+@Composable
+fun AvailableBalanceLabel(modifier: Modifier = Modifier) {
+    Text(
+        text = "AVAILABLE BALANCE",
+        style = CaptionMedium,
+        color = ContentSecondary,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
+}
